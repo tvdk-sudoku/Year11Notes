bf4a3a71dbfe7fc8bcb3422d44ce840b3d13113f
Grim4Reaper <Grim4Reaper@users.noreply.github.com>
2023-06-14 08:30:32 +0000

    deploy: 22165a84445ad27e82edaafcc8bba1267458f5d7

---
+++ notes/links.py
@@ -0,0 +1,14 @@
+import os
+
+
+with os.listdir("C:\\Users\\tajvd\\OneDrive - education.wa.edu.au\\School\\2023\\Notes\\content\\notes") as files:
+    for i in files:
+        print(i)
+        
+
+
+
+
+
+
+
