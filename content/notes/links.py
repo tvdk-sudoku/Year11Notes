cafe225b000566fcf4c914657bbd29e9f4b1de89
MQ-9 Reaper <taj.vanderklauw@student.education.wa.edu.au>
2023-06-15 09:08:29 +0800

    Testing

--- content/notes/links.py
+++ content/notes/links.py
@@ -1,14 +1,22 @@
 import os
 
+def replace_file_paths(directory, old_path, new_path):
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):  # Replace with the desired file extension
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r') as file:
+                content = file.read()
 
-with os.listdir("C:\\Users\\tajvd\\OneDrive - education.wa.edu.au\\School\\2023\\Notes\\content\\notes") as files:
-    for i in files:
-        print(i)
-        
+            updated_content = content.replace(old_path, new_path)
 
+            with open(file_path, 'w') as file:
+                file.write(updated_content)
 
+            print(f"Updated file: {filename}")
 
+# Usage example
+directory = 'C:/Users/tajvd/OneDrive - education.wa.edu.au/School/2023/Notes/content/notes'  # Replace with the actual directory path
+old_path = ''  # Replace with the old file path
+new_path = ''  # Replace with the new file path
 
-
-
-
+replace_file_paths(directory, old_path, new_path)
---
+++ test/run-schema.cmd
@@ -0,0 +1,1 @@
+wrangler d1 execute test --file="C:\Users\tajvd\OneDrive - education.wa.edu.au\School\2023\Notes\test\schema.sql"
