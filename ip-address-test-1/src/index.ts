907568019710696567ea109ebde44fbb74490abf
grim4reaper123 <grim4reaper123@gmail.com>
2023-08-04 17:01:37 +0800

    backup: 2023-08-04 17:01:37

--- ip-address-test-1/src/index.ts
+++
@@ -1,8 +0,0 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const clientIP = request.headers.get("cf-connecting-ip")
-  console.log(clientIP)
-}
